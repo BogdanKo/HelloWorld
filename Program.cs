d073d4f350d7c33d6bc31dd0fe4b3076e1be2907
KondrusikB <bogdan.kondrusik@gmail.com>
2016-02-14 16:25:48 +0200

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Debug ");
+            Console.WriteLine("Hello Read");
+            //Console.WriteLine("Using comment button is very convinient");
+            Console.ReadLine();
+        }
+    }
+}
